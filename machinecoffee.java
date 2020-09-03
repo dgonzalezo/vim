f8c063cbbea467ac7e6edb28dc76032c64dc5730
dgonzalezo <dgonzalezo@unal.edu.co>
2020-09-03 00:12:09 -0500

    test para usar git

---
+++ machinecoffee.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class machinecoffee{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Starting to make a coffee");
+        System.out.println("Grinding coffee beans");
+        System.out.println("Boiling water");
+        System.out.println("Mixing boiled water with crushed coffee beans");
+        System.out.println("Pouring coffee into the cup");
+        System.out.println("Pouring some milk into the cup");
+        System.out.println("Coffee is ready!");
+        System.out.println("Write how many cups of coffee you will need:");
+        scanner.close();
+    }
+}
